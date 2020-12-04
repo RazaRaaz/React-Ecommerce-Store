fa710cd937140c3148e83d774517f5bb4cbab710
raza <raza.0000.arj@gmail.com>
2020-12-04 11:46:28 +0500

    Add a navbar to the App and style it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.jsx'
-import {Route} from 'react-router-dom'
+import { Switch ,Route} from 'react-router-dom'
 import ShopPage from './pages/shop/shop.jsx'
-
+import Header from './components/header/header.jsx'
 
 
 function App() {
   return (
     <div className="App">
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
-      {/* <Route path='/shop/hats' component={HatsPage} /> */}
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        {/* <Route path='/shop/hats' component={HatsPage} /> */}
+      </Switch>
     </div>
   );
 }
